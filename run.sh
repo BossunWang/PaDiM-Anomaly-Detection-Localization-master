2d28004af1d758d8aea610bcbcf633bddb4dcdfa
Bossun Wang <vvmodouco@gmail.com>
2023-06-21 15:33:30 +0800

    update to train potholes dataset

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
-python main.py \
-  --data_path /media/glory/46845c74-37f7-48d7-8b72-e63c83fa4f68/ultralytics_datasets/mvtec_anomaly_detection \
+#python main.py \
+#  --data_path /media/glory/46845c74-37f7-48d7-8b72-e63c83fa4f68/ultralytics_datasets/mvtec_anomaly_detection \
+#  --arch resnet18
+
+python run_potholes.py \
+  --data_path /media/glory/46845c74-37f7-48d7-8b72-e63c83fa4f68/ultralytics_datasets/pothole600/training \
+  --save_path potholes_result \
   --arch resnet18
---
+++ run_potholes.py
@@ -0,0 +1,328 @@
+import random
+from random import sample
+import argparse
+import numpy as np
+import os
+import pickle
+from tqdm import tqdm
+from collections import OrderedDict
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+from sklearn.metrics import precision_recall_curve
+from sklearn.covariance import LedoitWolf
+from scipy.spatial.distance import mahalanobis
+from scipy.ndimage import gaussian_filter
+from skimage import morphology
+from skimage.segmentation import mark_boundaries
+import matplotlib.pyplot as plt
+import matplotlib
+import time
+
+import torch
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+import datasets.dataset as dataset
+from resnet import CustomResnet
+
+
+# device setup
+use_cuda = torch.cuda.is_available()
+device = torch.device('cuda' if use_cuda else 'cpu')
+
+
+def mahalanobis_torch(u, v, cov_inv):
+    delta = u - v
+    m = torch.dot(delta, torch.matmul(cov_inv, delta))
+    return torch.sqrt(m)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser('PaDiM')
+    parser.add_argument('--data_path', type=str, default='D:/dataset/mvtec_anomaly_detection')
+    parser.add_argument('--save_path', type=str, default='./mvtec_result')
+    parser.add_argument('--arch', type=str, choices=['resnet18', 'wide_resnet50_2'], default='wide_resnet50_2')
+    return parser.parse_args()
+
+
+def main():
+
+    args = parse_args()
+
+    # load model
+    if args.arch == 'resnet18':
+        t_d = 448
+        d = 100
+    elif args.arch == 'wide_resnet50_2':
+        t_d = 1792
+        d = 550
+
+    model = CustomResnet(args.arch, pretrained=True)
+    model.to(device)
+    model.eval()
+    random.seed(1024)
+    torch.manual_seed(1024)
+    if use_cuda:
+        torch.cuda.manual_seed_all(1024)
+
+    idx = torch.tensor(sample(range(0, t_d), d)).to(device)
+
+    os.makedirs(os.path.join(args.save_path, 'temp_%s' % args.arch), exist_ok=True)
+    fig, ax = plt.subplots(1, 2, figsize=(20, 10))
+    fig_img_rocauc = ax[0]
+    fig_pixel_rocauc = ax[1]
+
+    total_roc_auc = []
+    total_pixel_roc_auc = []
+
+    for class_name in dataset.CLASS_NAMES_POTHOLES:
+
+        train_dataset = dataset.PotholesDataset(args.data_path, class_name=class_name, is_train=True)
+        train_dataloader = DataLoader(train_dataset, batch_size=32, pin_memory=True)
+        test_dataset = dataset.PotholesDataset(args.data_path, class_name=class_name, is_train=False)
+        test_dataloader = DataLoader(test_dataset, batch_size=32, pin_memory=True)
+
+        train_outputs = OrderedDict([('layer1', []), ('layer2', []), ('layer3', [])])
+        test_outputs = OrderedDict([('layer1', []), ('layer2', []), ('layer3', [])])
+
+        # extract train set features
+        train_feature_filepath = os.path.join(args.save_path, 'temp_%s' % args.arch, 'train_%s.pkl' % class_name)
+        if not os.path.exists(train_feature_filepath):
+            for (x, _, _) in tqdm(train_dataloader, '| feature extraction | train | %s |' % class_name):
+                # model prediction
+                with torch.no_grad():
+                    outputs = model(x.to(device))
+                # get intermediate layer outputs
+                for k, v in zip(train_outputs.keys(), outputs):
+                    train_outputs[k].append(v.cpu().detach())
+
+            for k, v in train_outputs.items():
+                train_outputs[k] = torch.cat(v, 0)
+
+            # Embedding concat
+            embedding_vectors = train_outputs['layer1']
+            for layer_name in ['layer2', 'layer3']:
+                embedding_vectors = embedding_concat(embedding_vectors, train_outputs[layer_name])
+
+            # randomly select d dimension
+            embedding_vectors = torch.index_select(embedding_vectors, 1, idx.cpu())
+            # calculate multivariate Gaussian distribution
+            B, C, H, W = embedding_vectors.size()
+            embedding_vectors = embedding_vectors.view(B, C, H * W)
+            mean = torch.mean(embedding_vectors, dim=0).numpy()
+            cov_inv = torch.zeros(C, C, H * W).numpy()
+            I = np.identity(C)
+            for i in range(H * W):
+                # cov[:, :, i] = LedoitWolf().fit(embedding_vectors[:, :, i].numpy()).covariance_
+                cov = np.cov(embedding_vectors[:, :, i].numpy(), rowvar=False) + 0.01 * I
+                cov_inv[:, :, i] = np.linalg.inv(cov)
+            # save learned distribution
+            train_outputs = [mean, cov_inv]
+            with open(train_feature_filepath, 'wb') as f:
+                pickle.dump(train_outputs, f)
+        else:
+            print('load train set feature from: %s' % train_feature_filepath)
+            with open(train_feature_filepath, 'rb') as f:
+                train_outputs = pickle.load(f)
+
+        gt_list = []
+        gt_mask_list = []
+        test_imgs = []
+
+        # extract test set features
+        for (x, y, mask) in tqdm(test_dataloader, '| feature extraction | test | %s |' % class_name):
+            test_imgs.extend(x.cpu().detach().numpy())
+            gt_list.extend(y.cpu().detach().numpy())
+            gt_mask_list.extend(mask.cpu().detach().numpy())
+            # model prediction
+            start_inf_time = time.time()
+            with torch.no_grad():
+                outputs = model(x.to(device))
+            end_inf_time = time.time()
+            # get intermediate layer outputs
+            for k, v in zip(test_outputs.keys(), outputs):
+                test_outputs[k].append(v)
+            print(f'inference time:{end_inf_time - start_inf_time}')
+
+        start_fill_time = time.time()
+        for k, v in test_outputs.items():
+            test_outputs[k] = torch.cat(v, 0)
+
+        # Embedding concat
+        embedding_vectors = test_outputs['layer1']
+        for layer_name in ['layer2', 'layer3']:
+            embedding_vectors = embedding_concat(embedding_vectors, test_outputs[layer_name])
+
+        # randomly select d dimension
+        embedding_vectors = torch.index_select(embedding_vectors, 1, idx)
+        end_fill_time = time.time()
+        print(f'fill vector time:{end_fill_time - start_fill_time}')
+
+        start_dist_time = time.time()
+        # calculate distance matrix
+        B, C, H, W = embedding_vectors.size()
+        # print(f'embedding_vectors:{embedding_vectors.size()}')
+        embedding_vectors = embedding_vectors.view(B, C, H * W)
+        dist_list = []
+
+        # print(f"train_outputs[0]:{train_outputs[0].shape}")
+        # print(f"train_outputs[1]:{train_outputs[1].shape}")
+        # print(f"embedding_vectors:{embedding_vectors.shape}")
+        mean_matrix = torch.from_numpy(train_outputs[0]).to(device)
+        embedding_vectors_zero_mean = (embedding_vectors - mean_matrix).permute(0, 2, 1)
+        cov_inv_matrix = torch.from_numpy(train_outputs[1]).permute(2, 0, 1).to(device)
+
+        # print(f"embedding_vectors_zero_mean:{embedding_vectors_zero_mean.shape}")
+        # print(f"cov_inv_matrix:{cov_inv_matrix.shape}")
+        mul_mat = torch.einsum('bcd,abd->abc', cov_inv_matrix, embedding_vectors_zero_mean)
+        dist_list = torch.einsum('abc,abc->ab', embedding_vectors_zero_mean, mul_mat)
+        dist_list = dist_list.reshape(B, H, W).cpu().detach().numpy()
+        end_inf_time = time.time()
+        print(f'calculate distance time:{end_inf_time - start_dist_time}')
+
+        # for i in range(H * W):
+        #     mean = torch.from_numpy(train_outputs[0][:, i]).to(device)
+        #     conv_inv = torch.from_numpy(train_outputs[1][:, :, i]).to(device)
+        #     dist = [mahalanobis_torch(ev[:, i], mean, conv_inv).cpu().detach() for ev in embedding_vectors]
+        #     # conv_inv = np.linalg.inv(train_outputs[1][:, :, i])
+        #     # dist = [mahalanobis(sample[:, i], mean, conv_inv) for sample in embedding_vectors]
+        #     dist_list.append(dist)
+        # end_inf_time = time.time()
+        # print(f'inference time:{end_inf_time - start_dist_time}')
+        #
+        # dist_list = np.array(dist_list).transpose(1, 0).reshape(B, H, W)
+
+        # upsample
+        dist_list = torch.tensor(dist_list)
+        score_map = F.interpolate(dist_list.unsqueeze(1), size=x.size(2), mode='bilinear',
+                                  align_corners=False).squeeze().numpy()
+
+        # apply gaussian smoothing on the score map
+        for i in range(score_map.shape[0]):
+            score_map[i] = gaussian_filter(score_map[i], sigma=4)
+
+        # Normalization
+        max_score = score_map.max()
+        min_score = score_map.min()
+        scores = (score_map - min_score) / (max_score - min_score)
+
+        # calculate image-level ROC AUC score
+        img_scores = scores.reshape(scores.shape[0], -1).max(axis=1)
+        gt_list = np.asarray(gt_list)
+        fpr, tpr, _ = roc_curve(gt_list, img_scores)
+        img_roc_auc = roc_auc_score(gt_list, img_scores)
+        total_roc_auc.append(img_roc_auc)
+        print('image ROCAUC: %.3f' % (img_roc_auc))
+        fig_img_rocauc.plot(fpr, tpr, label='%s img_ROCAUC: %.3f' % (class_name, img_roc_auc))
+
+        # get optimal threshold
+        gt_mask = np.asarray(gt_mask_list)
+        precision, recall, thresholds = precision_recall_curve(gt_mask.flatten(), scores.flatten())
+        a = 2 * precision * recall
+        b = precision + recall
+        f1 = np.divide(a, b, out=np.zeros_like(a), where=b != 0)
+        threshold = thresholds[np.argmax(f1)]
+        print(f'threshold: {threshold}')
+
+        # calculate per-pixel level ROCAUC
+        fpr, tpr, _ = roc_curve(gt_mask.flatten(), scores.flatten())
+        per_pixel_rocauc = roc_auc_score(gt_mask.flatten(), scores.flatten())
+        total_pixel_roc_auc.append(per_pixel_rocauc)
+        print('pixel ROCAUC: %.3f' % (per_pixel_rocauc))
+
+        fig_pixel_rocauc.plot(fpr, tpr, label='%s ROCAUC: %.3f' % (class_name, per_pixel_rocauc))
+        save_dir = args.save_path + '/' + f'pictures_{args.arch}'
+        os.makedirs(save_dir, exist_ok=True)
+        plot_fig(test_imgs, scores, gt_mask_list, threshold, save_dir, class_name)
+
+    print('Average ROCAUC: %.3f' % np.mean(total_roc_auc))
+    fig_img_rocauc.title.set_text('Average image ROCAUC: %.3f' % np.mean(total_roc_auc))
+    fig_img_rocauc.legend(loc="lower right")
+
+    print('Average pixel ROCUAC: %.3f' % np.mean(total_pixel_roc_auc))
+    fig_pixel_rocauc.title.set_text('Average pixel ROCAUC: %.3f' % np.mean(total_pixel_roc_auc))
+    fig_pixel_rocauc.legend(loc="lower right")
+
+    fig.tight_layout()
+    fig.savefig(os.path.join(args.save_path, 'roc_curve.png'), dpi=100)
+
+
+def plot_fig(test_img, scores, gts, threshold, save_dir, class_name):
+    num = len(scores)
+    vmax = scores.max() * 255.
+    vmin = scores.min() * 255.
+    for i in range(num):
+        img = test_img[i]
+        img = denormalization(img)
+        gt = gts[i].transpose(1, 2, 0).squeeze()
+        heat_map = scores[i] * 255
+        mask = scores[i]
+        mask[mask > threshold] = 1
+        mask[mask <= threshold] = 0
+        kernel = morphology.disk(4)
+        mask = morphology.opening(mask, kernel)
+        mask *= 255
+        vis_img = mark_boundaries(img, mask, color=(1, 0, 0), mode='thick')
+        fig_img, ax_img = plt.subplots(1, 5, figsize=(12, 3))
+        fig_img.subplots_adjust(right=0.9)
+        norm = matplotlib.colors.Normalize(vmin=vmin, vmax=vmax)
+        for ax_i in ax_img:
+            ax_i.axes.xaxis.set_visible(False)
+            ax_i.axes.yaxis.set_visible(False)
+        ax_img[0].imshow(img)
+        ax_img[0].title.set_text('Image')
+        ax_img[1].imshow(gt, cmap='gray')
+        ax_img[1].title.set_text('GroundTruth')
+        ax = ax_img[2].imshow(heat_map, cmap='jet', norm=norm)
+        ax_img[2].imshow(img, cmap='gray', interpolation='none')
+        ax_img[2].imshow(heat_map, cmap='jet', alpha=0.5, interpolation='none')
+        ax_img[2].title.set_text('Predicted heat map')
+        ax_img[3].imshow(mask, cmap='gray')
+        ax_img[3].title.set_text('Predicted mask')
+        ax_img[4].imshow(vis_img)
+        ax_img[4].title.set_text('Segmentation result')
+        left = 0.92
+        bottom = 0.15
+        width = 0.015
+        height = 1 - 2 * bottom
+        rect = [left, bottom, width, height]
+        cbar_ax = fig_img.add_axes(rect)
+        cb = plt.colorbar(ax, shrink=0.6, cax=cbar_ax, fraction=0.046)
+        cb.ax.tick_params(labelsize=8)
+        font = {
+            'family': 'serif',
+            'color': 'black',
+            'weight': 'normal',
+            'size': 8,
+        }
+        cb.set_label('Anomaly Score', fontdict=font)
+
+        fig_img.savefig(os.path.join(save_dir, class_name + '_{}'.format(i)), dpi=100)
+        plt.close()
+
+
+def denormalization(x):
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    x = (((x.transpose(1, 2, 0) * std) + mean) * 255.).astype(np.uint8)
+    
+    return x
+
+
+def embedding_concat(x, y):
+    B, C1, H1, W1 = x.size()
+    _, C2, H2, W2 = y.size()
+    s = int(H1 / H2)
+    x = F.unfold(x, kernel_size=s, dilation=1, stride=s)
+    x = x.view(B, C1, -1, H2, W2)
+    z = torch.zeros(B, C1 + C2, x.size(2), H2, W2).to(x.device)
+    for i in range(x.size(2)):
+        z[:, :, i, :, :] = torch.cat((x[:, :, i, :, :], y), 1)
+    z = z.view(B, -1, H2 * W2)
+    z = F.fold(z, kernel_size=s, output_size=(H1, W1), stride=s)
+
+    return z
+
+
+if __name__ == '__main__':
+    main()
